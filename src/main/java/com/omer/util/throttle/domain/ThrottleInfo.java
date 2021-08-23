f5fa3b9cffab9b602a0f59953f3b00359373bc3a
omerhashmininjago <hashmi.omer@gmail.com>
2021-08-24 01:05:06 +0530

    Throttling utilities

--- src/main/java/com/omer/util/throttle/domain/ThrottleInfo.java
+++ src/main/java/com/omer/util/throttle/domain/ThrottleInfo.java
@@ -1,43 +1,17 @@
 package com.omer.util.throttle.domain;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
 public final class ThrottleInfo {
 
-    protected final String throttleKey;
-    protected final int throttleRate;
+    private final String throttleKey;
+    private final int throttleRate;
 
-    public ThrottleInfo(String throttleKey, int throttleRate) {
-        this.throttleKey = throttleKey;
-        this.throttleRate = throttleRate;
-    }
-
-    public int getThrottleRate() {
-        return throttleRate;
-    }
-
-    public String getThrottleKey() {
-        return throttleKey;
-    }
-
-    @Override
-    public String toString() {
-        return "ThrottleInfo{" +
-                "throttleKey='" + throttleKey + '\'' +
-                ", throttleRate=" + throttleRate +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ThrottleInfo that = (ThrottleInfo) o;
-        return throttleRate == that.throttleRate && throttleKey.equals(that.throttleKey);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(throttleKey, throttleRate);
-    }
 }
